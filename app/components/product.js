de93cc007d98b6c945a7b229ada22f842ec47cc5
nicholaskier <nicholaskier@gmail.com>
2021-04-08 15:56:41 -0500

    conditional images for headphone colors

--- app/components/product.js
+++ app/components/product.js
@@ -3,10 +3,6 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@glimmer/object'
 
 export default class ProductComponent extends Component {
-    @tracked number = 0;
-
-    @action
-    addNumber() {
-        this.number = this.number + 1;
+   
     }
 }
--- app/controllers/item.js
+++ app/controllers/item.js
@@ -1,4 +1,16 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ItemController extends Controller {
+    @tracked color = 'red';
+
+    get productImage() {
+        return `public/assets/images/beats-solo-${this.color}.png`
+    }
+
+    @action
+    onChangeColor(newColor) {
+        this.color = newColor
+    }
 }
